bc2d4ee592cc170ca4fce8e91ff40a5ed867635f
Natália Rodrigues <natytotherodrigues@gmail.com>
2024-08-17 16:29:23 -0300

    update schema

--- game/backend/1_schema.sql
+++ game/backend/1_schema.sql
@@ -22,19 +22,28 @@ CREATE TABLE personagem (
     idPersonagem SERIAL PRIMARY KEY,
     idSala INTEGER REFERENCES sala(idSala) DEFAULT 1,
     nomeP VARCHAR(50) NOT NULL,
+    hpMax INTEGER DEFAULT 100,
+    hpAtual INTEGER DEFAULT 100
+);
+
+--adicionar
+CREATE TABLE vitalidade(
+    idVitalidade SERIAL,
     nutricao INTEGER DEFAULT 100,
     hidratacao INTEGER DEFAULT 100,
     stamina INTEGER DEFAULT 100,
     calor INTEGER DEFAULT 50,
-    hpMax INTEGER DEFAULT 100,
-    hpAtual INTEGER DEFAULT 100,
-    dano INTEGER DEFAULT 0
+    dano INTEGER DEFAULT 0,
+    PRIMARY KEY (idVitalidade, idPersonagem),
+    FOREIGN KEY (idPersonagem) REFERENCES personagem (idPersonagem)
+    ON DELETE CASCADE
 );
 
 --corrigir
 CREATE TABLE distrito (
     idDistrito SERIAL,
     idPersonagem INTEGER NOT NULL,
+    popularidade INTEGER DEFAULT 0,
     agilidade INTEGER,
     forca INTEGER,
     nado INTEGER,
@@ -48,3 +57,34 @@ CREATE TABLE distrito (
     FOREIGN KEY (idPersonagem) REFERENCES personagem (idPersonagem)
     ON DELETE CASCADE
 );
+
+--corrigir
+CREATE TABLE inventario (
+    idInventario SERIAL PRIMARY KEY,
+    idPersonagem INTEGER NOT NULL,
+    capMax INTEGER DEFAULT 2,
+    capAtual INTEGER DEFAULT 0,
+    FOREIGN KEY (idPersonagem) REFERENCES personagem (idPersonagem)
+    ON DELETE CASCADE
+);
+
+--corrigir
+CREATE TABLE personagem_jogavel (
+    idPersonagem INTEGER PRIMARY KEY,
+    idDistrito INTEGER REFERENCES distrito(idDistrito)
+);
+
+--corrigir
+CREATE TABLE personagem_nao_jogavel (
+    idPersonagem SERIAL PRIMARY KEY,
+    nomeNPC VARCHAR(50) NOT NULL,
+    descricao TEXT DEFAULT ''
+);
+
+--corrigir
+CREATE TABLE animal (
+    idAnimal SERIAL PRIMARY KEY,
+    nomeA VARCHAR(50) NOT NULL,
+    idPersonagem INTEGER UNIQUE REFERENCES personagem (idPersonagem)
+)
+
